47fb8f0db0ec8cec4774362249c6c92ec27d79de
edi-marc <edi-marc@student.42roma.it>
2022-08-14 15:03:17 +0200

    Initial part finishedgit status! - The window is drawn correctly

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: edi-marc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 11:17:08 by edi-marc          #+#    #+#             */
-/*   Updated: 2022/08/14 14:08:20 by edi-marc         ###   ########.fr       */
+/*   Updated: 2022/08/14 14:57:02 by edi-marc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,12 @@ int	main(int argc, char **argv)
 	t_map	m;
 
 	is_valid(argc, argv, &m);
+	m.win.mlx = mlx_init();
 	if (!m.win.mlx)
 		print_exit_error(MALLOC_ERR_TXT, MALLOC_ERR, &m);
 	m.win.win = mlx_new_window(m.win.mlx, m.col * 64, m.row * 64, "So long");
 	init_win(&m);
 	fill_windows(&m);
+
+	mlx_loop(m.win.mlx);
 }
